0ee5022742cfcfad3c6b1f8268a2a2c5da7bfaac
matfire <30804999+matfire@users.noreply.github.com>
2017-08-17 17:46:35 +0200

    Add files via upload

---
+++ J06/ex04/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgassend <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/10 16:16:09 by mgassend          #+#    #+#             */
+/*   Updated: 2017/08/17 09:13:15 by mgassend         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_sort_ascii(char **val, int nb)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < (nb - 1))
+	{
+		j = 0;
+		while (val[i][j] == val[i + 1][j])
+			j++;
+		if (val[i][j] > val[i + 1][j])
+		{
+			tmp = val[i];
+			val[i] = val[i + 1];
+			val[i + 1] = tmp;
+			i = 0;
+		}
+		i++;
+	}
+}
+
+void	ft_sort_params(char **val, int nb)
+{
+	int i;
+	int y;
+
+	if (nb > 1)
+	{
+		i = 1;
+		while (i < nb)
+		{
+			y = 0;
+			while (val[i][y] != '\0')
+			{
+				ft_putchar(val[i][y]);
+				y++;
+			}
+			ft_putchar('\n');
+			i++;
+		}
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	ft_sort_ascii(argv, argc);
+	ft_sort_params(argv, argc);
+	return (0);
+}
